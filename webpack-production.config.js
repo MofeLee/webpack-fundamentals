6875a6700c7cafa02f2f457d57e7f13270d85d83
jsfeb26 <jsfeb26@gmail.com>
2015-11-01 15:18:32 -0500

    Add production build config and add strip loader

---
+++ webpack-production.config.js
@@ -0,0 +1,11 @@
+var WebpackStrip = require('strip-loader');
+var devConfig = require('./webpack.config.js');
+var stripLoader = {
+  test: [/\.js$/, /\.es6$/],
+  exclude: /node_modules/,
+  loader: WebpackStrip.loader('console.log') // strips out console.log statements
+}
+
+devConfig.module.loaders.push(stripLoader); // adds strip loader to dev loaders
+
+module.exports = devConfig; // uses most of dev config
