1b78ec5dace2ecdb78f20d4c637b6676124ee60c
jsfeb26 <jsfeb26@gmail.com>
2015-11-01 15:28:37 -0500

    Update project folder structure, set up webpack dev server to serve bundle from virutal directory so it's not checked in

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -3,7 +3,7 @@ var devConfig = require('./webpack.config.js');
 var stripLoader = {
   test: [/\.js$/, /\.es6$/],
   exclude: /node_modules/,
-  loader: WebpackStrip.loader('console.log') // strips out console.log statements
+  loader: WebpackStrip.loader('console.log', 'perfLog') // strips out console.log statements
 }
 
 devConfig.module.loaders.push(stripLoader); // adds strip loader to dev loaders
